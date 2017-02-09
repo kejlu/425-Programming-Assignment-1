b8eb0be7922fc6b29a87b8cd7ed7e934ed02123a
Kevin <klu12@umd.edu>
2017-02-08 23:23:20 -0500

    rusty with github lol

---
+++ Assets/Scripts/ReachedGoal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachedGoal : MonoBehaviour {
+
+	void Start () {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Goal")) {
+			Physics.gravity = new Vector3 (0, 10f, 0);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Tilt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour {
+
+	public Vector3 currentRot;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		currentRot = GetComponent<Transform> ().eulerAngles;
+
+		if (Input.GetAxis ("Horizontal") > 0.2 && (currentRot.z <= 11 || currentRot.z >= 349)) {
+			transform.Rotate (0, 0, -1);
+		}
+
+		if (Input.GetAxis ("Horizontal") < -0.2 && (currentRot.z <= 10 || currentRot.z >= 348)) {
+			transform.Rotate (0, 0, 1);
+		}
+
+		if (Input.GetAxis ("Vertical") > 0.2 && (currentRot.x <= 10 || currentRot.x >= 348)) {
+			transform.Rotate (1, 0, 0);
+		}
+
+		if (Input.GetAxis ("Vertical") < -0.2 && (currentRot.x <= 11 || currentRot.x >= 349)) {
+			transform.Rotate (-1, 0, 0);
+		}
+	}
+}
